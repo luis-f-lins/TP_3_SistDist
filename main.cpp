95ee15d7c019e74237992cfca562c7fe278c5f4e
Iuri Costermani <iuricostermani@hotmail.com>
2019-06-17 18:49:47 -0300

    Sockets not working

--- main.cpp
+++ main.cpp
@@ -25,10 +25,14 @@
 #include <string.h>
 #include <unistd.h> 
 #include <sys/socket.h> 
+#include <sys/types.h> 
 #include <stdlib.h> 
 #include <netinet/in.h> 
-#include <string.h> 
+#include <string> 
 #include <arpa/inet.h> 
+#include <semaphore.h>
+#include <string.h>
+#include <csignal>
 #define PORT 8080 
 
 using namespace std;
@@ -36,6 +40,13 @@ using namespace std;
 /*
  * 
  */
+
+sem_t disabled_mutex, leader_mutex, sent_msgs_count, recv_msgs_count, election_timer_mutex;
+clock_t election_timer = -1;
+bool disabled = false;
+bool leader_alive = false;
+int leader_pid = -1;
+pid_t pid = getpid();
 
 int getch(void) {
       int c=0;
@@ -59,44 +70,104 @@ int getch(void) {
 int sendingMessages();
 int recvMessages();
 
+
+int sendMessages(int code, int value = -1){
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    string msg; 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){ 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+   
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0){ 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    }
+
+        //connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){ 
+        printf("\nConnection error\n"); 
+        return -1; 
+    } 
+
+    if(value > 0)
+        msg = to_string(code) + "|" + to_string(pid) + "|" + to_string(value);
+    else
+        msg = to_string(code) + "|" + to_string(pid);
+        
+    // msg = string(15 - sizeof(msg), '0').append(msg);     
+
+    socklen_t len;
+    // sendto(sock, (const char *)msg.c_str(), strlen(msg.c_str()), 0, (const struct sockaddr *) &serv_addr,sizeof(serv_addr));
+    // printf("%s\n", msg.c_str());
+    send(sock , (const char *)msg.c_str(), 1024, 0);
+    close(sock);
+    sem_post(&sent_msgs_count);
+    return 0;
+}
+
 int keycommands(){ // Reads keypress at any time and prints 'Done!' if key is f, and ends program is key is g.
     char inp;
     
     while(inp!='g'){
         inp=getch();
     
-        if (inp=='f'){
-            cout << "Done! \n";
+        if (inp=='1'){
+            // Check leader; creates an interruption
+            raise(SIGUSR1);            
         }
+        else if(inp =='2'){
+            //disable process
+            sem_wait(&disabled_mutex);
+                disabled = true;
+            sem_post(&disabled_mutex);
+        }
+        else if(inp =='3'){
+            //enable process
+            sem_wait(&disabled_mutex);
+                disabled = false;
+            sem_post(&disabled_mutex);
+        }
+        else if(inp =='4'){
+            int sent_count, recv_count;
+            sem_getvalue(&sent_msgs_count, &sent_count);
+            sem_getvalue(&recv_msgs_count, &recv_count);
+            printf("Sent messages : %d\nReceived messages : %d\nCurrent leader: %d\n", sent_count, recv_count, leader_pid);
+        }
+        else if(inp =='5'){
+            sendMessages(1);
+        }
+
     }
     
     return 0;
 }
-
-int handleMessages(){
-    
-    return 0;
-}
-
-int sendingMessages(){
-    int server_fd, new_socket, valread; 
-    struct sockaddr_in address; 
+int recvMessages(){
+    int server_fd, new_socket, valread, sock; 
+    struct sockaddr_in address, cliaddress; 
     int opt = 1; 
+    int cliaddrlen = sizeof(cliaddress); 
     int addrlen = sizeof(address); 
-    char buffer[1024] = {0}; 
-    char *hello = "Hello from server"; 
+    char buffer[15] = {0}; 
+    int i = 0;
+    int msg[15];
+    char *value;
        
     // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) == 0) 
-    { 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){ 
         perror("socket failed"); 
         exit(EXIT_FAILURE); 
-    } 
+    }
        
-    // Forcefully attaching socket to the port 8080 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
-    { 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){ 
         perror("setsockopt"); 
+    // Forcefully attaching socket to the port 8080 
         exit(EXIT_FAILURE); 
     } 
     address.sin_family = AF_INET; 
@@ -104,71 +175,158 @@ int sendingMessages(){
     address.sin_port = htons( PORT ); 
        
     // Forcefully attaching socket to the port 8080 
-    if (::bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) 
-    { 
+    if (::bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0){ 
         perror("bind failed"); 
         exit(EXIT_FAILURE); 
+    }
+
+    if (listen(server_fd, 100) < 0){ 
+        printf("Listening error\n"); 
+        exit(1); 
     } 
-    if (listen(server_fd, 3) < 0) 
-    { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0) 
-    { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    } 
-    valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    send(new_socket , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
+
+
+    socklen_t len;
+    while(1){
+        // valread = recvfrom(server_fd, (char *)buffer, 1024, MSG_WAITALL, ( struct sockaddr *) &address, (socklen_t*)&addrlen);
+        if ((sock = accept(server_fd, (struct sockaddr *)&cliaddress, (socklen_t*)&cliaddrlen))<0){ 
+            printf("Accept error\n");   
+            exit(1); 
+        }
+        recv(sock , buffer, 1024, 0);
+
+        printf("Server recieved: %s\n",buffer);
+        value = strtok(buffer, "|");
+        while(value != NULL){
+           msg[i] = atoi(value);
+           value = strtok(NULL, "|");
+           i ++;
+        }
+        i = 0;
+
+        if(msg[1] != pid){
+
+            sem_post(&recv_msgs_count);
+
+            if(msg[0]==3){ //LIDER
+                printf("Received LEADER\n");            
+                sem_wait(&leader_mutex);
+                    leader_pid = msg[1];
+                sem_post(&leader_mutex);
+                sem_wait(&election_timer_mutex);
+                    election_timer = -1;
+                sem_post(&election_timer_mutex);
+
+            }
+
+            if(!disabled){
+
+                switch(msg[0]){
+                    case 1: //election 
+                        printf("Received ELECTION\n");
+                        if(msg[1] < pid){
+                            printf("Sending OK\n");
+                            sendMessages(2, msg[1]); //send OK to stop election
+                            sendMessages(1); //start new election
+                        }
+                        break; 
+                    case 2: //OK 
+                        if(msg[2] == pid){
+                            printf("Received OK\n");
+                            //reset election timer
+                            sem_wait(&election_timer_mutex);
+                                election_timer = -1;
+                            sem_post(&election_timer_mutex);
+
+                        }
+                        break;
+                    case 4: //ALIVE sends ALIVE-OK to leader
+                        if(leader_pid == pid){
+                            printf("Sending ALIVE\n");
+                            sendMessages(5, msg[1]);
+                        }
+                        break;
+                    case 5: //ALIVE-OK
+                        if(msg[2] == pid){
+                            printf("Received ALIVE-OK\n");
+                            leader_alive = true;
+                            sem_wait(&election_timer_mutex);
+                                election_timer = -1;
+                            sem_post(&election_timer_mutex);                            
+                        } 
+                        break; 
+                } 
+            }
+        }
+    }
     return 0; 
 }
 
-int recvMessages(){
-    int sock = 0, valread; 
-    struct sockaddr_in serv_addr; 
-    char *hello = "Hello from client"; 
-    char buffer[1024] = {0}; 
-    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) 
-    { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
-    } 
-   
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
-    { 
-        printf("\nInvalid address/ Address not supported \n"); 
-        return -1; 
-    } 
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        printf("\nConnection Failed \n"); 
-        return -1; 
-    } 
-    send(sock , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
-    valread = read( sock , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    return 0; 
-    
+int checkLeader(){
+    while(1){
+        printf("%ld\n", (clock() - election_timer));
+        if(leader_pid < 0 && election_timer < 0){
+            printf("Starting new election \n");
+            sendMessages(1); //starts new election
+            sem_wait(&election_timer_mutex);
+                election_timer = clock();
+            sem_post(&election_timer_mutex);
+
+        }    
+        else if(leader_pid < 0 && election_timer > 0 && (clock() - election_timer) > 100){
+            printf("Becoming new leader \n");
+            sendMessages(3); //declares as leader if no new messages
+            sem_wait(&leader_mutex);
+            sem_wait(&election_timer_mutex);
+                leader_pid = pid;
+                election_timer = -1;
+            sem_post(&leader_mutex);
+            sem_post(&election_timer_mutex);
+        }
+        else if(leader_pid > 0){
+            if(election_timer < 0){
+                if(leader_pid != pid){
+                    printf("Sending ALIVE \n");
+                    sendMessages(4, leader_pid); //sends alive msg
+                    sem_wait(&election_timer_mutex);
+                        election_timer = clock();
+                    sem_post(&election_timer_mutex);
+                }
+            }else if(election_timer > 0 && (clock() - election_timer) > 100){
+                printf("Sending LEADER \n");
+                sendMessages(3); 
+                sem_wait(&leader_mutex);
+                sem_wait(&election_timer_mutex);
+                    leader_pid = pid;
+                    election_timer = -1;
+                sem_post(&leader_mutex);
+                sem_post(&election_timer_mutex);
+            }
+        }
+        sleep(2);        
+    }
 }
-
-
 
 int main(int argc, char** argv) {
     thread myThreads[3];
-    
+    sem_init(&disabled_mutex, 0, 1); 
+    sem_init(&leader_mutex, 0, 1); 
+    sem_init(&election_timer_mutex, 0, 1); 
+    sem_init(&sent_msgs_count, 0, 0); 
+    sem_init(&recv_msgs_count, 0, 0); 
     myThreads[0] = thread(keycommands);
+    myThreads[1] = thread(recvMessages);
+    myThreads[2] = thread(checkLeader);
     
-    myThreads[0].join();
+    printf("pid %d\n", pid);
+
+    for(int i = 0; i < sizeof(myThreads); i++){
+        myThreads[i].join();
+    }
+    sem_destroy(&disabled_mutex); 
+    sem_destroy(&sent_msgs_count); 
+    sem_destroy(&recv_msgs_count); 
+    sem_destroy(&leader_mutex); 
     
     return 0;
 }
